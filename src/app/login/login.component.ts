82b7327eb81407afac6155f93f98f0098f0e0322
rcampion <campion.richard@gmail.com>
2019-06-18 23:56:33 -0400

    Change redirect

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -69,7 +69,7 @@ export class LoginComponent implements OnInit {
                     console.log('Successfully logged in.', account);
                     this.account.authenticated = true;
                     this.router.navigateByUrl('/home');
-                    window.location.href = '/contacts/home';
+                    window.location.href = '/contacts/about';
                     // window.location.reload();
                 },
 
