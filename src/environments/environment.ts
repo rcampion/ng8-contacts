79735c8a64501ee594133dd345945e444cb2242d
rcampion <campion.richard@gmail.com>
2019-07-09 01:21:11 -0400

    Point to the backend to the cloud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api_url: 'http://www.zdslogic-development.com:8080/ng8-spring-server/api'
+//  api_url: 'http://www.zdslogic-development.com:8080/ng8-spring-server/api'
+  api_url: 'http://www.zdslogic-development.com:8080/spring-boot-docker-server/api'
 };
 
 /*
